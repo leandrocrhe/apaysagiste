67064eced2271fbd4df9370e911ecfc76b8104d8
leandrocrhe <108886070+leandrocrhe@users.noreply.github.com>
2023-06-28 12:15:07 -0400

    footer complete modified:   index.html modified:   menu.html modified:   static/css/main.css modified:   static/css/main.css.map renamed:    static/img/bg-img-home-01.jpg -> static/img/banner_home.jpg deleted:    static/img/bg-05.jpg new file:   static/img/bg-locations.jpg deleted:    static/img/quote2.svg renamed:    static/img/services/semento.jpg -> static/img/services/cemento.jpg modified:   static/js/main.js new file:   static/scss/_footer.scss modified:   static/scss/_generals.scss new file:   static/scss/_home_banner.scss new file:   static/scss/_home_location.scss new file:   static/scss/_home_services.scss new file:   static/scss/_home_testimonials.scss new file:   static/scss/_home_warranty.scss new file:   static/scss/_mixin.scss modified:   static/scss/_nav.scss modified:   static/scss/_variables.scss modified:   static/scss/main.scss

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,23 @@ const navMenu = document.querySelector(".nav_menu");
 navToggle.addEventListener("click", function() {
   navMenu.classList.toggle("nav_menu-visible");
 });
+
+document.addEventListener('DOMContentLoaded', function(){
+  navFixed();
+});
+
+function navFixed() {
+
+  const navBar = document.querySelector('.nav_sm');
+
+  // Registrar Intersection Observer
+  const observer = new IntersectionObserver( function(entries) {
+    if(entries[0].isIntersecting) {
+      navBar.classList.remove('nav_sm-fixed');
+    } else {
+      navBar.classList.add('nav_sm-fixed');      
+    };
+  });
+  // Elemento a obserbar
+  observer.observe(document.querySelector('.observe'))
+}
