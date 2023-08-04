691f1e4fc811bbd14bff5f56ee264758c3b2cdf8
leandrocrhe <108886070+leandrocrhe@users.noreply.github.com>
2023-08-03 21:53:11 -0400

    Actualización en BTN Top new file:   about.html modified:   contact.html modified:   index.html modified:   menu.html modified:   services_01.html modified:   services_02.html modified:   services_03.html modified:   services_04.html modified:   services_05.html modified:   services_06.html modified:   services_07.html modified:   services_08.html modified:   services_09.html modified:   services_10.html modified:   static/css/main.css modified:   static/css/main.css.map renamed:    static/img/services/bg-services.jpg -> static/img/bg-about.jpg new file:   static/img/bg-contact.jpg modified:   static/js/main.js modified:   static/js/script.js new file:   static/scss/_about.scss modified:   static/scss/_banner.scss modified:   static/scss/_generals.scss modified:   static/scss/_home_warranty.scss modified:   static/scss/main.scss

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,22 @@
 const navToggle = document.querySelector(".nav_toggle");
 const navMenu = document.querySelector(".nav_menu");
+const btnFloat = document.querySelector(".btn_top");
+
+window.addEventListener('scroll', () => {
+  const services = document.querySelector('h1');
+  const mapServ = services.getBoundingClientRect();
+  if (mapServ.top >= -10) {
+    btnFloat.classList.add("btn_top-disabled");
+  } else {
+    btnFloat.classList.remove("btn_top-disabled"); 
+  }
+});
 
 navToggle.addEventListener("click", function() {
   navMenu.classList.toggle("nav_menu-visible");
 });
 
+/*
 document.addEventListener('DOMContentLoaded', function(){
   navFixed();
 });
@@ -24,3 +36,4 @@ function navFixed() {
   // Elemento a obserbar
   observer.observe(document.querySelector('.observe'))
 }
+*/
